2a43669c93e7a744cbd7db028a44efc2ea2361da
Kia Fathi <kiafathi@me.com>
2014-08-27 13:22:53 -0700

    [FEATURE] Implement more functionality to solutionPublisher

--- solutionPublisher.sh
+++ solutionPublisher.sh
@@ -4,6 +4,7 @@ echo "Hello World"
 
 mkdir tempStoreOfRepo
 git clone https://github.com/hackreactor/2014-07-toy-problems.git tempStoreOfRepo/
+
 if [ "$#" = 0 ]; then
   open ./tempStoreOfRepo
 fi
@@ -12,8 +13,16 @@ if [ "$#" = 1 ]; then
   open ./tempStoreOfRepo/$1/$1.js
 fi
 
+cd tempStoreOfRepo
+git checkout solution
+
+ 
 echo "Add your solution code..."
 echo "Are you ready to push the solution? (y/n)"
 read  finished_variable
+pwd
+echo "Pushing the solution branch to GitHub"
+git push origin solution
+
 echo "Removing the file"
 rm -rf ./tempStoreOfRepo/
