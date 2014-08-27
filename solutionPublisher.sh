55ede7d24c21f1421baeffa6834e60a462775411
Kia Fathi <kiafathi@me.com>
2014-08-27 14:36:52 -0700

    [FEATURE] Add ability to update both senior and junior repos

--- solutionPublisher.sh
+++ solutionPublisher.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 echo "$1"
-echo "Hello World"
+echo "Welcome to the Solution Publisher"
 
 mkdir tempStoreOfRepo
-git clone https://github.com/hackreactor/2014-07-toy-problems.git tempStoreOfRepo/
+git clone https://github.com/hackreactor/2014-07-toy-problems.git tempStoreOfRepoJunior/
+git clone https://github.com/hackreactor/2014-06-toy-problems.git tempStoreOfRepoSenior/
 
 if [ "$#" = 0 ]; then
-  open ./tempStoreOfRepo
+  open ./tempStoreOfRepoJunior
+  open ./tempStoreOfRepoSenior
 fi
 
 if [ "$#" = 1 ]; then  
-  open ./tempStoreOfRepo/$1/$1.js
+  open ./tempStoreOfRepoJunior/$1/$1.js
+  open ./tempStoreOfRepoSenior/$1/$1.js
+
 fi
 
-cd tempStoreOfRepo
+cd tempStoreOfRepoJunior
 git checkout solution
+cd ../tempStoreOfRepoSenior
+git checkout solution
+cd ..
 
  
 echo "Add your solution code..."
 echo "Are you ready to push the solution? (y/n)"
 read  finished_variable
-pwd
-echo "Pushing the solution branch to GitHub"
-git push origin solution
 
-echo "Removing the file"
-rm -rf ./tempStoreOfRepo/
+if [ finished_variable = "y" ]; then
+  echo "Pushing the solution branch to GitHub"
+  cd tempStoreOfRepoJunior
+  git push origin solution
+  cd ../tempStoreOfRepoSenior
+  git push origin solution
+  cd ..
+
+else
+  echo "Ok, Goodbye!"
+fi
+
+echo "Removing the temporary repository directory"
+rm -rf ./tempStoreOfRepoSenior/
+rm -rf ./tempStoreOfRepoJunior/
